770caadbd774983fca38b4f43825307a3cf54e13
Keith Ricker <keithricker@hotmail.com>
2015-07-16 17:34:55 -0500

    Update server-base-start.sh

--- server-base-start.sh
+++ server-base-start.sh
@@ -8,16 +8,16 @@
 # RUN /etc/my_init.d/00_regen_ssh_host_keys.sh
 
 # FOR LOCAL ENVIRONMENT ONLY
-if [ ! -z "$ENABLE_SSH"];
-	then
-	if [ ! -z "$SSH_USERNAME" ]; then sshuname=$SSH_USERNAME else sshuname=root; fi
-	if [ ! -z "$SSH_PASSWORD" ]; then sshpass=$SSH_PASSWORD else sshpass=password; fi
-	rm -f /etc/service/sshd/down
-	echo "${sshuname}:${sshpass}" | chpasswd
-	sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-	chmod 0755 /var/run/sshd
-	mkdir -p /root/.ssh/ && touch /root/.ssh/authorized_keys
-	sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+if [ "${ENABLE_SSH}" != "" ];
+then
+if [ ! -z "$SSH_USERNAME" ]; then sshuname=$SSH_USERNAME else sshuname=root; fi
+if [ ! -z "$SSH_PASSWORD" ]; then sshpass=$SSH_PASSWORD else sshpass=password; fi
+rm -f /etc/service/sshd/down
+echo "${sshuname}:${sshpass}" | chpasswd
+sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+chmod 0755 /var/run/sshd
+mkdir -p /root/.ssh/ && touch /root/.ssh/authorized_keys
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd;
 fi
 # END LOCAL ONLY
 
